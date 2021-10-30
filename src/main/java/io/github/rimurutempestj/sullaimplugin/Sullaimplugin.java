4bfe35288424c414f187989fe42a740ea05b68f0
rimurutempestj <93257307+rimurutempestj@users.noreply.github.com>
2021-10-30 12:08:52 +0900

    Update Sullaimplugin.java

--- src/main/java/io/github/rimurutempestj/sullaimplugin/Sullaimplugin.java
+++ src/main/java/io/github/rimurutempestj/sullaimplugin/Sullaimplugin.java
@@ -1,13 +1,21 @@
 package io.github.rimurutempestj.sullaimplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Sullaimplugin extends JavaPlugin {
+public final class Sullaimplugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
         Bukkit.getPluginCommand("waterorlavaslime").setTabCompleter(new tapcom());
         getLogger().info("슬라임 플러그인 켜짐");
         getCommand("testslime").setExecutor(new testslime()); //testslime
@@ -16,7 +24,17 @@ public final class Sullaimplugin extends JavaPlugin {
         getCommand("waterorlavaslime").setExecutor(new waterslime()); //waterorlavaslime
         getCommand("openmenu").setExecutor(new on()); //openmenu
     }
+    @EventHandler
+    public void onClick(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        Action a = e.getAction();
 
+        if(a == Action.LEFT_CLICK_AIR) {
+            p.sendMessage("You was left-clicked on air");
+        } else if(a == Action.LEFT_CLICK_BLOCK) {
+            p.sendMessage("You was left-clicked on block");
+        }
+    }
 
     @Override
     public void onDisable() {
